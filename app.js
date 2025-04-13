c64b7b87d971eb02509b9250042133ff7a7448cf
Kaung Myat Thway <kaungmyatthway1997@gmail.com>
2025-04-13 15:38:06 +0630

    ejs data dynamic output with foreach loop

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
-  res.render('index');
+
+  const blogs = [
+    {title : 'title1', text : 'hello 1'},
+    {title : 'title2', text : 'hello 2'},
+    {title : 'title3', text : 'hello 3'}
+  ]
+  res.render('index',{blogs});
 });
 
 app.get("/about", (req, res) => {
