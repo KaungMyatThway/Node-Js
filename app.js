33a0f24c566e48131e06ef789fc42ecff81445d6
Kaung Myat Thway <kaungmyatthway1997@gmail.com>
2025-04-13 15:14:59 +0630

    views engine add

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const app = express();
 
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
 app.get("/", (req, res) => {
-  res.sendFile('./views/index.html', {root: __dirname});
+  res.render('index');
 });
 
 app.get("/about", (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about');
 });
+
+app.use((req,res) => {
+    res.status(404);
+    res.render('404');
+
+})
 
 app.listen(3000);
