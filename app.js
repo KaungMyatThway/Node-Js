fa8a5d7f813ca3c35764a607112b7c30d8027859
Kaung Myat Thway <kaungmyatthway1997@gmail.com>
2025-04-14 20:40:26 +0630

    mongo db add

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const express = require("express");
 
 const app = express();
+const mongoose = require("mongoose");
+const Blog = require('./models/Blogs');
+
+//db connection
+
+let mongoUrl = "mongodb+srv://kaungmyatthway:test12345@cluster0.ya3n0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(mongoUrl).then(() => {
+    //after db connect start server
+    app.listen(3000);
+}).catch(e => {
+    console.log(e);
+})
 
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-app.get("/", (req, res) => {
+app.get('/add-blog', async (req,res) => {
+    let blog = new Blog({
+        title : 'BLog title 3',
+        intro : 'blog intro 3',
+        body : 'blog body 3'
+    })
 
-  const blogs = [
-    {title : 'title1', text : 'hello 1'},
-    {title : 'title2', text : 'hello 2'},
-    {title : 'title3', text : 'hello 3'}
-  ]
+   await blog.save();
+
+   res.send('blog saved');
+})
+
+app.get("/",async (req, res) => {
+
+ let blogs = await Blog.find().sort({createdAt : -1});
+
+//   const blogs = [
+//     {title : 'title1', text : 'hello 1'},
+//     {title : 'title2', text : 'hello 2'},
+//     {title : 'title3', text : 'hello 3'}
+//   ]
   res.render('index',{blogs,title : 'HOME'});
 });
 
@@ -25,4 +51,4 @@ app.use((req,res) => {
 
 })
 
-app.listen(3000);
+
